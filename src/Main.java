f97df869c13ae449263d0fd100994150374f29fe
Andjela Burns <kaz_ang@yahoo.com>
2024-12-15 13:31:00 +0100

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int amount = 28789;
+        int miles = 20;
+        String bonusMiles = ((amount / miles) + " " + "Miles");
+
+        System.out.println("Miles for purchased ticket");
+
+        System.out.println(bonusMiles);
+
+    }
+}
